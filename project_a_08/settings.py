d1f36e0a6ca302346876be7b874f47574b3dfe69
Jerry Liu <jyl3xf@virginia.edu>
2021-10-14 21:11:13 -0400

    changed database settings

--- project_a_08/settings.py
+++ project_a_08/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'project_a_08.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'd5ei91gb65r5dm',
@@ -92,7 +92,10 @@ DATABASES = {
         'HOST': 'ec2-44-197-94-126.compute-1.amazonaws.com',
         'PORT':  '5432',
     }
-}
+}'''
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
