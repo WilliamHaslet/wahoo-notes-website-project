18694ab0d480ba5f9b7106ecdd20a02e320bd445
Alex Pascocello <amp7fs@virginia.edu>
2021-10-17 14:00:20 -0400

    Added example test

--- main_app/tests.py
+++ main_app/tests.py
@@ -1,6 +1,5 @@
-from django.test import TestCase
-
-# Create your tests here.
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
 
 class DummyTestCase(TestCase):
     def setUp(self):
@@ -9,5 +8,17 @@ class DummyTestCase(TestCase):
     def test_dummy_test_case(self):
         self.assertEqual(1, 1)
 
-#Testing a push
-        
+class LoginTest(TestCase):
+
+    #Correctly Setup User
+    def setUp(self):
+        User = get_user_model()
+        user = User.objects.create(username='user')
+        user.set_password('password')
+        user.save()
+
+    def test_login(self):
+        c = Client()
+        logged_in = c.login(username='user', password='password')
+        self.assertTrue(logged_in)
+            
