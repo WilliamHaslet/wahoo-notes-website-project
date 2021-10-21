a1cf2ada3703653b20839606f7a797bf256a2ebe
Jerry Liu <jyl3xf@virginia.edu>
2021-10-21 18:13:11 -0400

    admin

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,8 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
+from django.contrib import admin
 # Create your models here.
 
 class Student(models.Model):
