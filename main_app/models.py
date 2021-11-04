b8c753cc2c06aa0faf9a63ce5d9f966648efc2bc
Jerry Liu <jyl3xf@virginia.edu>
2021-11-04 17:36:40 -0400

    removing extraneous features

--- main_app/models.py
+++ main_app/models.py
@@ -12,40 +12,12 @@ class Profile(models.Model):
     year = models.IntegerField(default=0)
     def __str__(self):
         return f'{self.user.username} Profile'
-    
-class Student(models.Model):
-    computing_id = models.CharField(max_length=30)
-    name = models.CharField(max_length=30)
-    year = models.IntegerField()
-    email = models.EmailField()
 
 class Class(models.Model):
     name = models.CharField(max_length=30)
     professor = models.CharField(max_length=30)
     time = models.CharField(max_length=30)
     semester = models.CharField(max_length=30)
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    def __str__(self):
-        return self.question_text
-    @admin.display(
-        boolean=True,
-        ordering='pub_date',
-        description='Published recently?',
-    )
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    def __str__(self):
-        return self.choice_text
 
 class UVAClass(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
--- main_app/views.py
+++ main_app/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth import logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
-from .models import Choice, Question, UVAClass, Student
+from .models import UVAClass, Profile
 from .forms import UserUpdateForm, ProfileUpdateForm
 
 @login_required
@@ -21,7 +21,7 @@ def profile(request):
             u_form.save()
             p_form.save()
             messages.success(request, f'Your account has been updated')
-            return redirect('editstudent')
+            return redirect('editprofile')
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
@@ -30,39 +30,20 @@ def profile(request):
         'p_form': p_form
     }
 
-    return render(request, 'main_app/editstudent.html')
+    return render(request, 'main_app/editprofile.html')
 
 class IndexView(generic.ListView):
-    model = Student
+    model = Profile
     template_name = 'main_app/index.html'
     context_object_name = 'profile'
-
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'main_app/detail.html'
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
-
-
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'main_app/results.html'
 
 class UVAClassView(generic.ListView):
     model = UVAClass
     template_name = 'main_app/uvaclass.html'
 
-class StudentView(generic.ListView):
-    model = Student
+class EditProfileView(generic.ListView):
+    model = Profile
     template_name = 'main_app/editprofile.html'
-
-class EditStudentView(generic.ListView):
-    model = Student
-    template_name = 'main_app/editstudent.html'
     context_object_name = 'profile'
 
 class UVAClassListView(generic.ListView):
@@ -74,24 +55,6 @@ class UVAClassListView(generic.ListView):
         Filters classes by user
         """
         return UVAClass.objects.filter(user=self.request.user)
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'main_app/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('main_app:results', args=(question.id,)))
 
 def addClass(request):
     if request.method=='POST':
@@ -109,7 +72,7 @@ def addClass(request):
             messages.error(request, "Blank Submission! You must fill out the entire form.")
     return render(request, 'main_app/uvaclass.html')
 
-def submitEditedStudent(request):
+def submitEditedProfile(request):
     if request.method=='POST':
         if request.POST.get('studentComputingID') and request.POST.get('studentYear'):
             request.user.profile.computing_id=request.POST.get('studentComputingID')
@@ -118,7 +81,7 @@ def submitEditedStudent(request):
             messages.success(request, "Successfully Submitted!")
         else:
             messages.error(request, "Blank Submission! You must submit all fields.")
-    return render(request, 'main_app/editstudent.html')
+    return render(request, 'main_app/editprofile.html')
 
 def logout_view(request):
     logout(request)
