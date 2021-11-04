172a965b958cc3b1335971520371dce6f18699c4
Jerry Liu <jyl3xf@virginia.edu>
2021-11-04 18:45:12 -0400

    added day

--- main_app/models.py
+++ main_app/models.py
@@ -8,17 +8,18 @@ from django.contrib.auth.models import User
 
 class Class(models.Model):
     id = models.IntegerField(default=0, primary_key=True)
-    department = models.CharField(max_length=30, default="None")
+    subject = models.CharField(max_length=30, default="None")
     code = models.CharField(max_length=30, default="None")
     section = models.CharField(max_length=30, default="None")
     name = models.CharField(max_length=30, default="None")
     professor = models.CharField(max_length=30, default="None")
     size = models.IntegerField(default=0)
+    day = models.CharField(max_length=30, default="None")
     start_time = models.TimeField(default="00:00:00")
     end_time = models.TimeField(default="00:00:00")
     semester = models.CharField(max_length=30, default="None")
     def __str__(self):
-        return f"{self.department} {self.code}, section {self.section}"
+        return f"{self.subject} {self.code}, section {self.section}"
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
