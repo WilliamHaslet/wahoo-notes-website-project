37890961e0b2e3b352da30824e3a2e2700176ee2
Jerry Liu <jyl3xf@virginia.edu>
2021-11-04 18:35:54 -0400

    added many to many relationship

--- main_app/models.py
+++ main_app/models.py
@@ -6,13 +6,6 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 # Create your models here.
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    computing_id = models.CharField(max_length=30, default='NULL')
-    year = models.IntegerField(default=0)
-    def __str__(self):
-        return f'{self.user.username} Profile'
-
 class Class(models.Model):
     id = models.IntegerField(default=0, primary_key=True)
     department = models.CharField(max_length=30, default="None")
@@ -27,12 +20,10 @@ class Class(models.Model):
     def __str__(self):
         return f"{self.department} {self.code}, section {self.section}"
 
-class UVAClass(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    id_text = models.CharField(max_length=200)
-    studentyr_text = models.CharField(max_length=200)
-    classname_text = models.CharField(max_length=200)
-    classtime_text = models.CharField(max_length=200)
-    classinst_text = models.CharField(max_length=200)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    computing_id = models.CharField(max_length=30, default='NULL')
+    year = models.IntegerField(default=0)
+    classes = models.ManyToManyField(Class)
     def __str__(self):
-        return self.classname_text
+        return f'{self.user.username} Profile'
