2734c9d72b0f15844e2d08aa3dd4ab9ad7a832dc
Jerry Liu <jyl3xf@virginia.edu>
2021-11-18 17:13:36 -0500

    changed assignment page to also include assignment due date

--- main_app/views.py
+++ main_app/views.py
@@ -87,6 +87,7 @@ def addAssignment(request):
         new_assignment.name = request.POST.get('assignment_name')
         new_assignment.class_name = request.POST.get('class_name')
         new_assignment.description = request.POST.get('description')
+        new_assignment.due_date = request.POST.get('due_date')
         new_assignment.profile = request.user.profile
         new_assignment.save()
         #request.user.profile.assignments.add(new_assignment)
