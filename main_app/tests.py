36a8a27a145a0a9adef44dcc3cedd75a91ba6920
sc8mnc <89964194+sc8mnc@users.noreply.github.com>
2021-11-30 04:51:13 -0500

    added profile, assignment, and classes test cases

--- main_app/tests.py
+++ main_app/tests.py
@@ -1,11 +1,15 @@
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from django.http import HttpRequest
+from django.template.loader import render_to_string
 
-from main_app.models import Class
+from main_app.models import Class, Assignment
+from main_app.forms import ProfileUpdateForm
+from main_app.views import ListClassesView
 
+#Login and logout
 class LoginTest(TestCase):
-
     #Correctly Setup User
     def setUp(self):
         User = get_user_model()
@@ -34,6 +38,7 @@ class LogoutTest(TestCase):
         c.logout()
         self.assertFalse(user.is_anonymous)
 
+#Classes
 import json
 from enum import IntEnum
 
@@ -90,3 +95,94 @@ class AddClassTest(TestCase):
         c = testUser.profile.classes.get(id=course.id)
         self.assertTrue(c.name == course.name)
         self.assertTrue(Class.objects.get(name=course.name) == c)
+
+class RemoveClassTest(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        user = User.objects.create(username='user1')
+        user.set_password('password')
+        user.save()
+
+    def test_removeclass(self):
+        User = get_user_model()
+        testUser = User.objects.get(username='user1')
+        
+        course = createClass(3000)
+        
+        testUser.profile.classes.add(course)
+        testUser.profile.save()
+        testUser.profile.classes.remove(course)
+        testUser.profile.save()
+        self.assertFalse(testUser.profile.classes.exists())
+
+#Assignments
+def createAssignment(profile):
+    newAssign = Assignment()
+    newAssign.id = 0
+    newAssign.profile = profile
+    newAssign.name = "Test Assignment"
+    newAssign.description = "Just a test"
+    newAssign.points = 100
+    newAssign.class_name = "CS3240"
+    newAssign.release_date = "11/1/21"
+    newAssign.due_date = "1/19/38"
+    newAssign.save()
+    return newAssign
+
+class AddAssignmentTest(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        user = User.objects.create(username='user1')
+        user.set_password('password')
+        user.save()
+
+    def test_addassignment(self):
+        User = get_user_model()
+        testUser = User.objects.get(username='user1')
+        assignment = createAssignment(testUser.profile)
+        assignment.save()
+        
+        a = Assignment.objects.get(id=assignment.id)
+        self.assertTrue(a.profile == assignment.profile)
+        self.assertTrue(a.name == assignment.name)
+        self.assertTrue(a.description == assignment.description)
+        self.assertTrue(a.class_name == assignment.class_name)
+        self.assertTrue(a.due_date == assignment.due_date)
+
+class RemoveAssignmentTest(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        user = User.objects.create(username='user1')
+        user.set_password('password')
+        user.save()
+
+    def test_removeassignment(self):
+        User = get_user_model()
+        testUser = User.objects.get(username='user1')
+        assignment = createAssignment(testUser.profile)
+        assignment.save()
+        assigntest = Assignment.objects.get(id=assignment.id)
+        assigntest.delete()
+        self.assertFalse(Assignment.objects.exists())
+
+#Profile
+class EditProfileTest(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        user = User.objects.create(username='user1')
+        user.set_password('password')
+        user.save()
+
+    def test_editprofile(self):
+        User = get_user_model()
+        testUser = User.objects.get(username='user1')
+        request = HttpRequest()
+        request.POST = {
+            "computing_id": "tes7er",
+            "year": 2021,
+        }
+
+        form = ProfileUpdateForm(request.POST, instance=testUser.profile)
+        form.save()
+        self.assertTrue(testUser.profile.computing_id == "tes7er")
+        self.assertTrue(testUser.profile.year == 2021)
