19089f9cd32789b2e2b556dcb58fd44fa3e42ba2
Jerry Liu <jyl3xf@virginia.edu>
2021-12-04 23:03:53 -0500

    fixing csp srcs

--- project_a_08/settings.py
+++ project_a_08/settings.py
@@ -43,21 +43,10 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
 CSP_DEFAULT_SRC = ("'self'",)
-CSP_STYLE_SRC = ("'self'", "stackpath.bootstrapcdn.com", "fonts.googleapis.com", "cdn.jsdelivr.net", "fonts.googleapis.com", "'unsafe-inline'",)
-CSP_SCRIPT_SRC = ("'self'",
-    "ajax.cloudflare.com",
-    "static.cloudflareinsights.com",
-    "www.google-analytics.com",
-    "ssl.google-analytics.com",
-    "cdn.ampproject.org",
-    "www.googletagservices.com",
-    "pagead2.googlesyndication.com",
-    "cdn.jsdelivr.net",)
-CSP_FONT_SRC = ("'self'", "fonts.gstatic.com",)
-CSP_IMG_SRC = ("'self'",
-    "www.google-analytics.com",
-    "raw.githubusercontent.com",
-    "googleads.g.doubleclick.net",)
+CSP_STYLE_SRC = ("'self'", "cdn.jsdelivr.net", "fonts.googleapis.com", "'unsafe-inline'",)
+CSP_SCRIPT_SRC = ("'self'", "cdn.jsdelivr.net",)
+CSP_FONT_SRC = ("'self'", "cdn.jsdelivr.net",)
+CSP_IMG_SRC = ("'self'", "icon-library.com",)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'wahoo-notes.herokuapp.com']
 
