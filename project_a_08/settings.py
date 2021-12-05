e16478479e843a3292b832a052b82eff8c62d5f3
Jerry Liu <jyl3xf@virginia.edu>
2021-12-04 22:33:34 -0500

    csp middleware

--- project_a_08/settings.py
+++ project_a_08/settings.py
@@ -80,6 +80,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'csp.middleware.CSPMiddleware',
 ]
 
 ROOT_URLCONF = 'project_a_08.urls'
